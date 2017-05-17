98ec744e3d656a765e5a6e9b9271e690be7630b8
snwint <wfeldt@opensuse.org>
2017-05-17 14:43:44 +0200

    remove last remaining old storage reference
    'timezone' now also runs on the installed system. (It will crash when
    run as non-root user due to storage-ng requiring root.)

--- timezone/src/clients/timezone.rb
+++ timezone/src/clients/timezone.rb
@@ -137,22 +137,9 @@ module Yast
       # create the wizard dialog
       Wizard.OpenOKDialog
 
-      # wrapper for storage calls, returns nil of yast2-storage is not installed
-      targets = Convert.convert(
-        WFM.call("wrapper_storage", ["GetTargetMap"]),
-        :from => "any",
-        :to   => "map <string, map>"
-      )
-      if targets != nil
-        partitions = Convert.convert(
-          WFM.call("wrapper_storage", ["GetWinPrimPartitions", [targets]]),
-          :from => "any",
-          :to   => "list <map>"
-        )
-        if partitions != nil && Ops.greater_than(Builtins.size(partitions), 0)
-          Timezone.windows_partition = true
-          Builtins.y2milestone("windows partition found")
-        end
+      if Timezone.system_has_windows?
+        Timezone.windows_partition = true
+        Builtins.y2milestone("windows partition found")
       end
 
       result = TimezoneDialog({})
