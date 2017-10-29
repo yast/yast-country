374937eb3b79d44dada520d1cc8dc48a766e9489
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2017-10-29 19:53:03 +0000

    refactor: avoid to use block code in a .map

--- keyboard_rewrite/src/lib/y2_keyboard/keyboard_layout.rb
+++ keyboard_rewrite/src/lib/y2_keyboard/keyboard_layout.rb
@@ -18,8 +18,8 @@ module Y2Keyboard
 
     def self.all
       raw_layouts = Cheetah.run("localectl", "list-keymaps", stdout: :capture)
-      layout_codes = raw_layouts.lines.map { |string| string.split.first }
-      codes_with_description = layout_codes.select { |code| LAYOUT_CODE_DESCRIPTIONS.key?(code) }
+      codes = raw_layouts.lines.map(&:strip)
+      codes_with_description = codes.select { |code| LAYOUT_CODE_DESCRIPTIONS.key?(code) }
       codes_with_description.map { |code| KeyboardLayout.new(code, LAYOUT_CODE_DESCRIPTIONS[code]) }
     end
   end
