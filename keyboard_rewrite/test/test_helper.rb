000fd695c8b4a8dc39e1d0febb29ae9e9a175a45
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-04-03 19:40:52 +0100

    remove code from test_helper for coverage

--- keyboard_rewrite/test/test_helper.rb
+++ keyboard_rewrite/test/test_helper.rb
@@ -6,25 +6,6 @@ require "yast"
 require "yast/rspec"
 require_relative "keyboard_spec_helper"
 
-if ENV["COVERAGE"]
-  require "simplecov"
-  SimpleCov.start do
-    add_filter "/test/"
-  end
-
-  # for coverage we need to load all ruby files
-  Dir["#{SRC_PATH}/lib/**/**/*.rb"].each { |f| require_relative f }
-
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  end
-end
-
 RSpec.configure do |config|
   config.include KeyboardSpecHelper    # custom helpers
 end
