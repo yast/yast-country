d651109d12f2dab7842a3cee439c1f8552d37659
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-04-03 20:30:39 +0100

    remove unnecessary code from rewrite test_helper

--- keyboard_rewrite/test/test_helper.rb
+++ keyboard_rewrite/test/test_helper.rb
@@ -1,28 +1,6 @@
 require_relative "../../test/test_helper.rb"
 require_relative "keyboard_spec_helper"
 
-SRC_PATH = File.expand_path("../../src", __FILE__)
-DATA_PATH = File.join(File.expand_path(File.dirname(__FILE__)), "data")
-
-if ENV["COVERAGE"]
-  require "simplecov"
-  SimpleCov.start do
-    add_filter "/test/"
-  end
-
-  # for coverage we need to load all ruby files
-  Dir["#{SRC_PATH}/lib/**/**/*.rb"].each { |f| require_relative f }
-
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
-    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-      SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
-    ]
-  end
-end
-
 RSpec.configure do |config|
   config.include KeyboardSpecHelper    # custom helpers
 end
