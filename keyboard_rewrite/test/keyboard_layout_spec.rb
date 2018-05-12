21bf2a6b58204feafe069d979629ebf7fd2c0622
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-05-12 19:51:55 +0100

    extract helper method

--- keyboard_rewrite/test/keyboard_layout_spec.rb
+++ keyboard_rewrite/test/keyboard_layout_spec.rb
@@ -59,16 +59,11 @@ describe Y2Keyboard::KeyboardLayout do
     subject(:keyboard_layout) { Y2Keyboard::KeyboardLayout }
     
     it "returns the current used keyboard layout" do
-      allow(Cheetah).to receive(:run).with("localectl", "status", stdout: :capture).and_return(
-        "   System Locale: LANG=en_US.UTF-8\n" \
-        "       VC Keymap: gb\n" \
-        "       X11 Layout: gb\n" \
-        "       X11 Model: microsoftpro\n" \
-        "       X11 Options: terminate:ctrl_alt_bksp\n")
-       expected_layouts = ["es", "gb", "us"]
-      given_layouts(expected_layouts)
+      current_selected_layout_code = "gb"
+      given_layouts(["es", current_selected_layout_code, "us"])
+      given_a_current_layout(current_selected_layout_code)
 
-      expect(keyboard_layout.get_current_layout().code).to eq("gb")
+      expect(keyboard_layout.get_current_layout().code).to eq(current_selected_layout_code)
     end
   end
 end
--- keyboard_rewrite/test/keyboard_spec_helper.rb
+++ keyboard_rewrite/test/keyboard_spec_helper.rb
@@ -5,6 +5,17 @@ module KeyboardSpecHelper
     ).and_return(layouts_to_return.join("\n"))
   end
 
+  def given_a_current_layout(code)
+    allow(Cheetah).to receive(:run)
+      .with("localectl", "status", stdout: :capture)
+      .and_return(
+        "   System Locale: LANG=en_US.UTF-8\n" \
+        "       VC Keymap: #{code}\n" \
+        "       X11 Layout: #{code}\n" \
+        "       X11 Model: microsoftpro\n" \
+        "       X11 Options: terminate:ctrl_alt_bksp\n")
+  end
+
   def mock_ui_events(*events)
     allow(Yast::UI).to receive(:UserInput).and_return(*events)
   end
