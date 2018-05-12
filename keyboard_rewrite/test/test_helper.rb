c14c45eac36738c7b7fe45915df687a706a1ac90
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-05-12 18:57:21 +0100

    allow use fit to filter tests to execute

--- keyboard_rewrite/test/test_helper.rb
+++ keyboard_rewrite/test/test_helper.rb
@@ -3,5 +3,7 @@ require_relative "keyboard_spec_helper"
 
 RSpec.configure do |config|
   config.include KeyboardSpecHelper    # custom helpers
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 end
 
