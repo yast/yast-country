b80fad51083306d10307b0df7c4bd33a523b2a7d
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-05-17 19:57:58 +0100

    Keyboard client now uses SystemdStrategy insted of KeyboardLayout

--- keyboard_rewrite/src/lib/y2keyboard/clients/keyboard.rb
+++ keyboard_rewrite/src/lib/y2keyboard/clients/keyboard.rb
@@ -1,11 +1,12 @@
 require_relative "../dialogs/layout_selector"
+require_relative "../strategies/systemd_strategy"
 
 module Y2Keyboard
   module Clients
     # Simple client to run LayoutSelector.
     class Keyboard
       def self.run
-        layouts = Y2Keyboard::KeyboardLayout.all
+        layouts = Y2Keyboard::Strategies::SystemdStrategy.all
         Y2Keyboard::Dialogs::LayoutSelector.new(layouts).run
       end
     end
