c4940e06c6204cfc1b8cd203d44fcf31165641f6
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-05-17 20:39:34 +0100

    Refactor: convert class methods into instace methods

--- keyboard_rewrite/src/lib/y2keyboard/strategies/systemd_strategy.rb
+++ keyboard_rewrite/src/lib/y2keyboard/strategies/systemd_strategy.rb
@@ -12,26 +12,18 @@ module Y2Keyboard
         "us" => "English (US)"
       }.freeze
 
-      def self.all
+      def all
         raw_layouts = Cheetah.run("localectl", "list-keymaps", stdout: :capture)
         codes = raw_layouts.lines.map(&:strip)
         codes_with_description = codes.select { |code| LAYOUT_CODE_DESCRIPTIONS.key?(code) }
         codes_with_description.map { |code| KeyboardLayout.new(code, LAYOUT_CODE_DESCRIPTIONS[code]) }
       end
 
-      def all
-        SystemdStrategy.all
-      end
-
-      def self.apply_layout(keyboard_layout)
+      def apply_layout(keyboard_layout)
         Cheetah.run("localectl", "set-keymap", keyboard_layout.code)
       end
 
-      def apply_layout(keyboard_layout)
-        SystemdStrategy.apply_layout(keyboard_layout)
-      end
-
-      def self.load_layout(keyboard_layout)
+      def load_layout(keyboard_layout)
         Cheetah.run("setxkbmap", keyboard_layout.code) if !Yast::UI.TextMode
         begin
           Cheetah.run("loadkeys", keyboard_layout.code) if Yast::UI.TextMode
@@ -41,36 +33,20 @@ module Y2Keyboard
         end
       end
 
-      def load_layout(keyboard_layout)
-        SystemdStrategy.load_layout(keyboard_layout)
-      end
-
-      def self.current_layout
+      def current_layout
         find_layout_with(current_layout_code)
       end
 
-      def current_layout
-        SystemdStrategy.current_layout
-      end
-
-      def self.find_layout_with(code)
+      def find_layout_with(code)
         all.find { |x| x.code == code }
       end
 
-      def find_layout_with(code)
-        SystemdStrategy.find_layout_with(code)
-      end
-
-      def self.current_layout_code
+      def current_layout_code
         output = Cheetah.run("localectl", "status", stdout: :capture)
         output.lines.map(&:strip).find { |x| x.start_with?("VC Keymap:") }.split.last
       end
 
-      def current_layout_code
-        SystemdStrategy.current_layout_code
-      end
-
-      private_class_method :current_layout_code, :find_layout_with
+      private :current_layout_code, :find_layout_with
     end
   end
 end
