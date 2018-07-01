053970aa56870771a63e02309cb48694a545e806
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-07-01 10:21:10 +0100

    KeyboardLayout.all returns a lists of keyboard layouts

--- keyboard_rewrite/src/lib/y2keyboard/keyboard_layout.rb
+++ keyboard_rewrite/src/lib/y2keyboard/keyboard_layout.rb
@@ -3,10 +3,24 @@ module Y2Keyboard
   class KeyboardLayout
     attr_reader :code
     attr_reader :description
+    @@strategy
+    @@layout_definitions
 
     def initialize(code, description)
       @code = code
       @description = description
+    end
+
+    def self.all
+      @@layout_definitions.map { |x| KeyboardLayout.new(x["code"], x["description"]) }
+    end
+
+    def self.use(strategy)
+      @@strategy = strategy
+    end
+
+    def self.layout_definitions(layout_definitions)
+      @@layout_definitions = layout_definitions
     end
   end
 end
---
+++ keyboard_rewrite/test/keyboard_layout_spec.rb
@@ -0,0 +1,23 @@
+require_relative "./test_helper"
+require "y2keyboard/keyboard_layout"
+require "y2keyboard/strategies/systemd_strategy"
+
+describe Y2Keyboard::KeyboardLayout do
+  subject(:keyboard_layout) { Y2Keyboard::KeyboardLayout }
+
+  describe ".all" do
+    subject(:all_layouts) { keyboard_layout.all}
+
+    it "returns a lists of keyboard layouts" do
+      layout_codes = ["es", "fr-latin1", "us", "uk"]
+      strategy = double(Y2Keyboard::Strategies::SystemdStrategy, :codes => layout_codes)
+      keyboard_layout.use(strategy)
+      keyboard_layout.layout_definitions(layout_definitions)
+
+      expect(all_layouts).to be_an(Array)
+      expect(all_layouts).to all(be_an(Y2Keyboard::KeyboardLayout))
+      layout_codes_loaded = all_layouts.map(&:code)
+      expect(layout_codes_loaded).to match_array(layout_codes)
+    end
+  end
+end
