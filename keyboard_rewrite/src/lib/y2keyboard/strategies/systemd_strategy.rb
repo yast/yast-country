bc8a150a6e887008d22a2d60c2d40d0a207d4d41
Joaquín Yeray Martín de la Nuez <joaquinmnsm@gmail.com>
2018-11-16 21:11:28 +0000

    remove unused code

--- keyboard_rewrite/src/lib/y2keyboard/strategies/systemd_strategy.rb
+++ keyboard_rewrite/src/lib/y2keyboard/strategies/systemd_strategy.rb
@@ -24,7 +24,6 @@ module Y2Keyboard
   module Strategies
     # Class to deal with systemd keyboard configuration management.
     class SystemdStrategy
-      include Yast::Logger
 
       def initialize(layout_definitions)
         @layout_definitions = layout_definitions
@@ -42,27 +41,6 @@ module Y2Keyboard
       # @param keyboard_layout [KeyboardLayout] the keyboard layout to apply in the system.
       def apply_layout(keyboard_layout)
         Cheetah.run("localectl", "set-keymap", keyboard_layout.code)
-      end
-
-      # Load x11 or virtual console keys on the fly.
-      # @param keyboard_layout [KeyboardLayout] the keyboard layout to load.
-      def load_layout(keyboard_layout)
-        load_x11_layout(keyboard_layout) if !Yast::UI.TextMode
-        begin
-          Cheetah.run("loadkeys", keyboard_layout.code) if Yast::UI.TextMode
-        rescue Cheetah::ExecutionFailed => e
-          log.info(e.message)
-          log.info("Error output:    #{e.stderr}")
-        end
-      end
-
-      # Load x11 keys on the fly.
-      # @param keyboard_layout [KeyboardLayout] the keyboard layout to load.
-      def load_x11_layout(keyboard_layout)
-        output = Cheetah.run("/usr/sbin/xkbctrl", keyboard_layout.code, stdout: :capture)
-        arguments = get_value_from_output(output, "\"Apply\"").tr("\"", "")
-        setxkbmap_array_arguments = arguments.split.unshift("setxkbmap")
-        Cheetah.run(setxkbmap_array_arguments)
       end
 
       # @return [KeyboardLayout] the current keyboard layout in the system.
@@ -83,7 +61,7 @@ module Y2Keyboard
         output.lines.map(&:strip).find { |x| x.start_with?(property_name) }.split(":", 2).last
       end
 
-      private :current_layout_code, :find_layout_with, :get_value_from_output, :load_x11_layout
+      private :current_layout_code, :find_layout_with, :get_value_from_output
     end
   end
 end
